50757e8a12a86013a151b1a0d1668d85744d84a0
Mark Phipps <mark.phipps0@gmail.com>
2020-10-14 07:30:45 +0100

    Parameterize connection details Update READMEs

--- README.md
+++ README.md
@@ -2,4 +2,13 @@
 Provides resource bundles of translated text via a rest-ish API
 
 
+The following properties are required:
 
+    -Djdbc.driver=**db driver class name** 
+    -Djdbc.url=< **arts db path** > 
+    -Djdbc.username= 
+    -Djdbc.password=
+    -Dserver.port=**http port**
+
+*e.g.*
+```-Djdbc.driver=org.postgresql.Driver -Djdbc.url=jdbc:postgresql://localhost:5432/arts -Djdbc.username=postgres -Djdbc.password= -Dserver.port=8081```
--- src/main/java/uk/ac/ox/ctsu/arts/translationservice/config/DataSourceConfig.java
+++ src/main/java/uk/ac/ox/ctsu/arts/translationservice/config/DataSourceConfig.java
@@ -1,5 +1,6 @@
 package uk.ac.ox.ctsu.arts.translationservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,14 +9,22 @@ import javax.sql.DataSource;
 
 @Configuration
 public class DataSourceConfig {
+    @Value( "${jdbc.driver}" )
+    private String driverClass;
+    @Value( "${jdbc.url}" )
+    private String url;
+    @Value( "${jdbc.username}" )
+    private String username;
+    @Value( "${jdbc.password}" )
+    private String password;
 
     @Bean
     public DataSource getDataSource() {
-        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName("org.postgresql.Driver");
-        dataSourceBuilder.url("jdbc:postgresql://localhost:5432/arts");
-        dataSourceBuilder.username("postgres");
-        dataSourceBuilder.password("");
-        return dataSourceBuilder.build();
+        return DataSourceBuilder.create()
+            .driverClassName(driverClass)
+            .url(url)
+            .username(username)
+            .password(password)
+            .build();
     }
 }
