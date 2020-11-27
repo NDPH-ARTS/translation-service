5bb775c3645ef3a05b80631ec9f41b4bb7098407
Mark Phipps <mark.phipps0@gmail.com>
2020-11-27 08:20:27 +0000

    Turn off security for translation reads Add translations for login, logout

--- src/main/java/uk/ac/ox/ctsu/arts/translationservice/config/SecurityConfig.java
+++ src/main/java/uk/ac/ox/ctsu/arts/translationservice/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package uk.ac.ox.ctsu.arts.translationservice.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,7 +11,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-            .antMatchers(HttpMethod.OPTIONS, "/translation/**").permitAll()
+            .antMatchers( "/translation/**").permitAll()
             .anyRequest().authenticated()
             .and().oauth2ResourceServer(oauth2 -> oauth2.jwt());
     }
